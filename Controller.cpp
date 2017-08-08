f8a0e30f0a719b0b886ab221e0938cd15119d1e8
hwieun <gk1vk@naver.com>
2017-08-08 20:15:02 +0900

    cpp add

--- Controller.cpp
+++ Controller.cpp
@@ -1,5 +1,18 @@
 #include "Controller.h"
 
+Termios Controller::NonBlockingTerminalMode() {
+	struct termios org_term;
+	tcgetattr(STDIN_FILENO, &org_term);
+	struct termios new_term;
+	new_term.c_lflag &= ~(ECHO | ICANON);
+	new_term.c_cc[VMIN] = 0;
+	new_term.c_cc[VTIME] = 0;
+	tcsetattr(STDIN_FILENO, TCSANOW, &new_term);
+	return org_term;
+}
+void Controller::ResetNonBlockingTerminalMode(Termios m_old_termio) {
+	tcsetattr(STDIN_FILENO, TCSANOW, &m_old_termio);
+}
 Controller::Controller()
 {
 	
@@ -22,7 +35,19 @@ int Controller::setView(View *pView){
     return true;
 }
 
-
+inline char Controller::GetNonBlockingInput() {
+	if (kbhit()) {
+		char input = (char)getch();
+		if (input != 224) //스페이스바 
+			return input;
+		else //방향키일때
+		{
+			input = (char)getch();
+			return input; //위 : 72 아래 : 80 오른쪽 : 77 왼쪽 : 75
+		}
+	}
+	return 0;
+}
 void Controller::CheckInput()
 {
 	char input_key = GetNonBlockingInput(); //input ascii value return
